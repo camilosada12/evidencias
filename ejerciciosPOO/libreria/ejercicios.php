9c63c1d66cd022298c60dfee1febccc2aced5f7f
camilosada12 <161945491+camilosada12@users.noreply.github.com>
2024-09-19 20:00:33 -0500

    Update ejercicios.php

--- ejerciciosPOO/libreria/ejercicios.php
+++ ejerciciosPOO/libreria/ejercicios.php
@@ -63,4 +63,77 @@ class ControlOperaciones {
         }
     }
 }
+
+class nomina{
+    private $valorDias;
+    private $DiasTrabajados;
+    public $salarioM = 1300000;
+    public $sueldo;
+    public $pension;
+    public $arl;
+    public $retencion;
+    public $sueldoTotal;
+    public $subtrasporte;
+    public $deducibles;
+    public $salud;
+
+    public function setValorDias($valorDias) {
+        $this->valorDias = $valorDias;
+    }
+
+    public function getValorDias() {
+        return $this->valorDias;
+    }
+
+    public function setDiasTrabajados($DiasTrabajados) {
+        $this->DiasTrabajados = $DiasTrabajados;
+    }
+
+    public function getDiasTrabajados() {
+        return $this->DiasTrabajados;
+    }
+
+    public function salario(){
+        $this->sueldo = $this->valorDias * $this->DiasTrabajados;
+        return $this->sueldo;
+    }
+    public function saludP(){
+        $this->salud = $this->sueldo * 0.12;
+        return $this->salud;
+    }
+    public function pensionP(){
+        $this->pension = $this->sueldo * 0.16;
+        return $this->pension;
+    }
+    public function arlP(){
+        $this->arl = $this->sueldo * 0.052;
+        return $this->arl;
+    }
+    public function retencionP(){
+        if($this->sueldo > $this->salarioM *4){
+            $this->retencion = $this->sueldo * 0.004;
+        }else{
+            $this->retencion = 0;
+        }
+        return $this->retencion;
+    }
+
+    public function subtrasporteP(){
+        if($this->sueldo < $this->salarioM ){
+            $this->subtrasporte = 114000;
+        }else{
+            $this->subtrasporte = 0;
+        }
+        return $this->subtrasporte;
+    }
+    public function deduciblesP(){
+       $this->deducibles =  $this->salud + $this->pension + $this->arl;
+        return $this->deducibles;
+    }
+    public function sueldoP(){
+        $this->sueldoTotal =  ($this->sueldo + $this->subtrasporte) - ($this->retencion + $this->deducibles);
+         return $this->sueldoTotal;
+     }
+}
 ?>
+
