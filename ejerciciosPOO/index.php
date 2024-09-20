fb9c864749b4494a33d20aeefa87a921ef3fc935
camilosada12 <161945491+camilosada12@users.noreply.github.com>
2024-09-19 20:00:55 -0500

    Update index.php

--- ejerciciosPOO/index.php
+++ ejerciciosPOO/index.php
@@ -11,6 +11,11 @@ $numeroDos->setNumeroDos(20);
 
 $operador = new ControlOperaciones();
 
+$salarioPersona = new nomina();
+
+$salarioPersona->setValorDias(20000);
+$salarioPersona->setDiasTrabajados(20);
+
 ?>
 
 <!DOCTYPE html>
@@ -29,6 +34,14 @@ $operador = new ControlOperaciones();
     echo "Resta: " . $operador->resta($numeroUno->getNumeroUno(), $numeroDos->getNumeroDos()) . "<br>";
     echo "Multiplicación: " . $operador->multi($numeroUno->getNumeroUno(), $numeroDos->getNumeroDos()) . "<br>";
     echo "División: " . $operador->division($numeroUno->getNumeroUno(), $numeroDos->getNumeroDos()) . "<br>";
+    echo "Sueldo Persona: " . $salarioPersona->salario() . "<br>";
+    echo "salud: " . $salarioPersona->saludP() . "<br>";
+    echo "pension: " . $salarioPersona->pensionP() . "<br>";
+    echo "arl: " . $salarioPersona->arlP() . "<br>";
+    echo "retencion: " . $salarioPersona->retencionP() . "<br>";
+    echo "subtrasporte: " . $salarioPersona->subtrasporteP() . "<br>";
+    echo "deducibles: " . $salarioPersona->deduciblesP() . "<br>";
+    echo "sueldoTotal: " . $salarioPersona->sueldoP() . "<br>";
     ?>
 </body>
 </html>
